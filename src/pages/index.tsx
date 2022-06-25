d47e8f1ad69d19748ef112fd933dd89836285f8c
aleksa-stojsic <stojsic.aleksa@gmail.com>
2022-06-25 19:18:26 +0200

    Feat: PWA iOS install prompt

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,8 @@
 import type { NextPage } from 'next';
+import dynamic from 'next/dynamic';
+const PWAPrompt = dynamic(() => import('react-ios-pwa-prompt' as any), {
+  ssr: false
+});
 import Layout from '../components/Layout';
 import Recipe from '../components/Recipe';
 import useFetch from '../hooks/useFetch';
@@ -9,20 +13,22 @@ const Home: NextPage = () => {
   const { data, loading, fetchData } = useFetch(apiUrl);
 
   return (
-    <Layout>
-      <h1 className='my-1 mt-8'>Hungry?</h1>
-      <h2 className='my-1'>Can&apos;t decide what to eat?</h2>
-      <h3 className='my-1'>We got you covered!</h3>
-      <button
-        className='btn btn-primary btn-block md:w-64 my-2 text-lg'
-        aria-label='Get a random recipe'
-        onClick={fetchData}
-      >
-        Generate Next Meal
-      </button>
-      <Recipe data={data} loading={loading} />
-    </Layout>
+    <>
+      <Layout>
+        <h1 className='my-1 mt-8'>Hungry?</h1>
+        <h2 className='my-1'>Can&apos;t decide what to eat?</h2>
+        <h3 className='my-1'>We got you covered!</h3>
+        <button
+          className='btn btn-primary btn-block md:w-64 my-2 text-lg'
+          aria-label='Get a random recipe'
+          onClick={fetchData}
+        >
+          Generate Next Meal
+        </button>
+        <Recipe data={data} loading={loading} />
+      </Layout>
+      <PWAPrompt />
+    </>
   );
 };
-
 export default Home;
