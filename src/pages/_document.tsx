b92c1b389f2d6733b9f4374c9c84c4e7bad73605
aleksa-stojsic <stojsic.aleksa@gmail.com>
2022-06-25 15:14:47 +0200

    Fix: maybe a fix for flash before switching to a theme

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,6 +10,54 @@ import Document, {
 const APP_NAME = 'Next Meal';
 const APP_DESCRIPTION = 'Generate a random recipe for your next meal';
 
+function setInitialColorMode() {
+  function getInitialColorMode() {
+    const preference = window.localStorage.getItem('theme');
+
+    const hasExplicitPreference = typeof preference === 'string';
+
+    /**
+     * If the user has explicitly chosen light or dark,
+     * use it. Otherwise, this value will be null.
+     */
+
+    if (hasExplicitPreference) {
+      return preference;
+    }
+
+    // If there is no saved preference, use a media query
+
+    const mediaQuery = '(prefers-color-scheme: dark)';
+
+    const mql = window.matchMedia(mediaQuery);
+
+    const hasImplicitPreference = typeof mql.matches === 'boolean';
+
+    if (hasImplicitPreference) {
+      return mql.matches ? 'dark' : 'light';
+    }
+
+    // default to 'light'.
+
+    return 'light';
+  }
+
+  const colorMode = getInitialColorMode();
+
+  // add HTML attribute if dark mode
+
+  if (colorMode === 'coffee') {
+    document.documentElement.setAttribute('data-theme', 'coffee');
+  } else {
+    document.documentElement.setAttribute('data-theme', 'bumblebee');
+  }
+}
+// our function needs to be a string
+
+const blockingSetInitialColorMode = `(function() {
+          ${setInitialColorMode.toString()}
+          setInitialColorMode();
+  })()`;
 class MyDocument extends Document {
   static async getInitialProps(
     ctx: DocumentContext
@@ -47,6 +95,11 @@ class MyDocument extends Document {
           {/* PWA end */}
         </Head>
         <body>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: blockingSetInitialColorMode
+            }}
+          ></script>
           <Main />
           <NextScript />
         </body>
