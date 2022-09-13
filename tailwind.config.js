5a403a2e7055565fbc5fe86ace5787653745e5f5
aleksastojsic <64015178+aleksa-codes@users.noreply.github.com>
2022-09-13 16:19:39 +0200

    fix: hover when supported

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,9 @@ module.exports = {
     './src/pages/**/*.{js,ts,jsx,tsx}',
     './src/components/**/*.{js,ts,jsx,tsx}'
   ],
+  future: {
+    hoverOnlyWhenSupported: true
+  },
   theme: {
     extend: {
       colors: {
